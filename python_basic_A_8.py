2ac49c3066982a12e603343582df58cd8c0682e3
InsightRoots <insight.roots.inc@gmail.com>
2022-01-24 17:56:36 +0900

    結果にcommit
    A-9終了

--- python_basic_A_8.py
+++ python_basic_A_8.py
@@ -1,8 +1,12 @@
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
-print("Name: " + users_info[0][0] + ", Age: " + str(users_info[0][1]))
-print("Name: " + users_info[1][0] + ", Age: " + str(users_info[1][1]))
-print("Name: " + users_info[2][0] + ", Age: " + str(users_info[2][1]))
+# print("Name: " + users_info[0][0] + ", Age: " + str(users_info[0][1]))
+# print("Name: " + users_info[1][0] + ", Age: " + str(users_info[1][1]))
+# print("Name: " + users_info[2][0] + ", Age: " + str(users_info[2][1]))
 
 # "Name: " Bob ", Age: "   79
 # これでいいのか。繰り返しが使えるのでは、ひとつずつ取り出しているからなにかつかえるのでは
+for fi in users_info:
+    print("Name: " + fi[0] + ", Age: " + str(fi[1]))
+
+# できたーー！！！
