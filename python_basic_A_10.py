30bd3dbe96d1bb23ed10fa653c48dc1707a94aee
InsightRoots <insight.roots.inc@gmail.com>
2022-01-29 08:49:32 +0900

    営業終了

--- python_basic_A_10.py
+++ python_basic_A_10.py
@@ -1,13 +1,10 @@
 import random
-
-
 # dice_me = [1,2,3,4,5,6]
 
 def dice():
     print(random.randint(1, 6))
-
-
 dice()
 
 # dice() 関数を実装してください
 # 実装するって・・・・・なんだ？定義して使えるようにしろってこと？これでいいのか？
+
--- python_basic_A_4.py
+++ python_basic_A_4.py
@@ -1,3 +1,13 @@
 members = ["Bob", "Tom", "Ken"]
 print(members[0])
 print(members[1])
+
+# 課題解説　例
+print(members[:2])
+
+# 課題解説　例　
+print('\n'.join(member for member in ["Bob", "Tom", "Ken"] if member == "Bob" or member == "Tom"))
+print('\n'.join(member for member in members if member == "Bob" or member == "Tom"))
+
+# 課題解説　例　
+print(members[0], members[1])
